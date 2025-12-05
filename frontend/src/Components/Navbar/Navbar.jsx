ad6715ac8137e9848fa8bae353d485c18dc88d9f
Ken <kenimorante@gmail.com>
2025-12-05 18:38:13 +0800

    Change the logo

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useRef, useState, useEffect } from 'react';
 import './Navbar.css';
-import bike from '../Assets/bike.png';
+import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 import { Link } from 'react-router-dom';
 import { HomeContext } from '../../Context/HomeContext';
@@ -58,8 +58,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="nav-logo">
-        <img src={bike} alt="" />
-        <p>totomotorworx</p>
+        <img src={logo} alt="" /> 
       </div>
       <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
       <ul ref={menuRef} className="nav-menu">
